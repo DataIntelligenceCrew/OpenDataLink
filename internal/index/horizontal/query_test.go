3192b67a634d7a2234c63307641af50c2464bc83
Justin Fargnoli <justinfargnoli@gmail.com>
2020-07-28 18:07:47 -0400

    Setup query_test.go

---
+++ internal/index/horizontal/query_test.go
@@ -0,0 +1,28 @@
+package horizontal
+
+import (
+	"testing"
+
+	"opendatalink/internal/database"
+)
+
+func buildIndex(t *testing.T) Index {
+	path, err := database.Path()
+	if err != nil {
+		t.Fatal(err)
+	}
+	db, err := database.New(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	index, err := BuildMetadataIndex(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return index
+}
+
+// func TestQuery(t *testing.T) {
+// 	index := buildIndex(t)
+// 	index.Query()
+// }
