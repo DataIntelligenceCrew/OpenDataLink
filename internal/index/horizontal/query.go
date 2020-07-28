6e680a949dc0e4313068959bcf8f5d6f749c1573
Justin Fargnoli <justinfargnoli@gmail.com>
2020-07-28 17:14:07 -0400

    Extract datasetID from Index.QueryRaw()

--- internal/index/horizontal/query.go
+++ internal/index/horizontal/query.go
@@ -7,8 +7,29 @@ type Index struct {
 	index *simhashlsh.CosineLsh
 }
 
-// Query finds the ids of approximate nearest neighbour candidates, in
+// QueryRaw finds the ids of approximate nearest neighbour candidates, in
 // un-sorted order, given the query point.
-func (i Index) Query(query []float64) []string {
+func (i Index) QueryRaw(query []float64) []string {
 	return i.index.Query(query)
+}
+
+// QueryResult is the deserialized result of an element of Index.Query()
+type QueryResult struct {
+	DatasetID string
+	Value     string
+}
+
+// BuildQueryResult builds a QueryResult given a query result in string form
+func BuildQueryResult(queryResult string) QueryResult {
+	return QueryResult{queryResult[:10], queryResult[10:]}
+}
+
+// Query finds the QueryResult's of approximate nearest neighbour candidates, in
+// un-sorted order, given the query point.
+func (i Index) Query(query []float64) []QueryResult {
+	var queryResults []QueryResult
+	for _, v := range i.QueryRaw(query) {
+		queryResults = append(queryResults, BuildQueryResult(v))
+	}
+	return queryResults
 }
