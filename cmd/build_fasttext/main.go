24935333302cd59a3cc5de85ea8d17b77b022368
Paul Ouellette <pouellet@u.rochester.edu>
2020-10-26 15:51:54 -0400

    Use ekzhu/go-fasttext in build_fasttext

---
+++ cmd/build_fasttext/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ekzhu/go-fasttext"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	ft := fasttext.NewFastText("fasttext.sqlite")
+	defer ft.Close()
+
+	if err := ft.BuildDB(os.Stdin); err != nil {
+		log.Fatal(err)
+	}
+}
