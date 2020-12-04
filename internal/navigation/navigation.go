0c05de47668938385c7e6e76b77a435cadf874cd
Paul Ouellette <pouellet@u.rochester.edu>
2020-12-04 13:39:19 -0500

    Complete building of initial organization

--- internal/navigation/navigation.go
+++ internal/navigation/navigation.go
@@ -1,10 +1,12 @@
 package navigation
 
 import (
+	"container/heap"
+
 	"github.com/DataIntelligenceCrew/OpenDataLink/internal/database"
 	"github.com/DataIntelligenceCrew/OpenDataLink/internal/vec32"
 	"github.com/DataIntelligenceCrew/go-faiss"
-
+	"gonum.org/v1/gonum/graph"
 	"gonum.org/v1/gonum/graph/simple"
 )
 
@@ -31,7 +33,7 @@ func newMergedNode(id int64, a, b *node) *node {
 	vec := make([]float32, embeddingDim)
 	vec32.Add(vec, a.vector)
 	vec32.Add(vec, b.vector)
-	vec32.Scale(vec, 1/2)
+	vec32.Scale(vec, 0.5)
 	vec32.Normalize(vec)
 
 	datasets := make(map[string]bool)
@@ -43,16 +45,17 @@ func newMergedNode(id int64, a, b *node) *node {
 	return &node{id, vec, datasets}
 }
 
-type graph struct {
+type tableGraph struct {
 	*simple.DirectedGraph
+	root graph.Node
 }
 
-func newGraph() *graph {
-	return &graph{simple.NewDirectedGraph()}
+func newGraph() *tableGraph {
+	return &tableGraph{simple.NewDirectedGraph(), nil}
 }
 
 // addDatasetNodes creates nodes for the datasets and adds them to the graph.
-func (g *graph) addDatasetNodes(db *database.DB) error {
+func (g *tableGraph) addDatasetNodes(db *database.DB) error {
 	rows, err := db.Query(`
 	SELECT dataset_id, emb
 	FROM metadata_vectors
@@ -84,8 +87,19 @@ func (g *graph) addDatasetNodes(db *database.DB) error {
 	return rows.Err()
 }
 
+// addMergedNode creates a new merged node from a and b and adds it to the graph
+// with edges to a and b.
+func (g *tableGraph) addMergedNode(a, b *node) *node {
+	id := g.NewNode().ID()
+	node := newMergedNode(id, a, b)
+	g.AddNode(node)
+	g.SetEdge(g.NewEdge(node, a))
+	g.SetEdge(g.NewEdge(node, b))
+	return node
+}
+
 // vectors returns the vectors of the nodes in g and the corresponding IDs.
-func (g *graph) vectors() (vectors []float32, ids []int64) {
+func (g *tableGraph) vectors() (vectors []float32, ids []int64) {
 	for it := g.Nodes(); it.Next(); {
 		node := it.Node().(*node)
 		vectors = append(vectors, node.vector...)
@@ -97,10 +111,16 @@ func (g *graph) vectors() (vectors []float32, ids []int64) {
 // index is an index over the vectors of nodes in a graph.
 type index struct {
 	idx *faiss.Index
-	g   *graph
+	g   *tableGraph
 }
 
-func buildIndex(g *graph) (*index, error) {
+// nodePair stores the similarity of two nodes.
+type nodePair struct {
+	a, b   *node
+	cosine float32
+}
+
+func buildIndex(g *tableGraph) (*index, error) {
 	idx, err := faiss.IndexFactory(embeddingDim, "IDMap,Flat", faiss.MetricInnerProduct)
 	if err != nil {
 		return nil, err
@@ -139,48 +159,98 @@ func (idx *index) remove(nodes ...*node) error {
 	return nil
 }
 
-// closestPair returns the pair of nodes that are most similar.
-// a and b will not be nil unless there was an error.
-func (idx *index) closestPair() (a, b *node, err error) {
+// query returns a nodePair containing the given node and its nearest neighbor.
+func (idx *index) query(n *node) (*nodePair, error) {
+	cos, ids, err := idx.idx.Search(n.vector, 1)
+	if err != nil {
+		return nil, err
+	}
+	res := idx.g.Node(ids[0]).(*node)
+
+	return &nodePair{n, res, cos[0]}, nil
+}
+
+// query_indexed is like query, but if the query node is in the index, the
+// result will not be the query itself.
+func (idx *index) query_indexed(n *node) (*nodePair, error) {
+	cos, ids, err := idx.idx.Search(n.vector, 2)
+	if err != nil {
+		return nil, err
+	}
+	// Usually the first result will be the query itself, but the query could be
+	// the second result if two datasets have the same embedding vectors.
+	// In this case, use the first result.
+	id, sim := ids[1], cos[1]
+	if id == n.id {
+		id = ids[0]
+		sim = cos[0]
+	}
+	// The next line panics if id is -1 (no result).
+	res := idx.g.Node(id).(*node)
+
+	return &nodePair{n, res, sim}, nil
+}
+
+// allPairs queries the index with all indexed nodes.
+func (idx *index) allPairs() ([]*nodePair, error) {
 	// Query vectors and IDs
 	vecs, qids := idx.g.vectors()
-	// Result IDs and corresponding cosine similarity
-	cos, ids, err := idx.idx.Search(vecs, 1)
+
+	cos, ids, err := idx.idx.Search(vecs, 2)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	var maxCos float32 = -2
-	var maxi int
-	for i, c := range cos {
-		if c > maxCos {
-			maxCos = c
-			maxi = i
+	results := make([]*nodePair, len(qids))
+	for i, qid := range qids {
+		a := idx.g.Node(qid).(*node)
+		id, sim := ids[i*2+1], cos[i*2+1]
+		if id == a.id {
+			id = ids[i*2]
+			sim = cos[i*2]
 		}
+		b := idx.g.Node(id).(*node)
+
+		results[i] = &nodePair{a, b, sim}
 	}
-	a = idx.g.Node(qids[maxi]).(*node)
-	b = idx.g.Node(ids[maxi]).(*node)
-	if a == nil || b == nil {
-		panic("a or b is nil")
-	}
-	return
+	return results, nil
 }
 
 func (idx *index) delete() {
 	idx.idx.Delete()
 }
 
+// priorityQueue is used to get the closest pair of nodes.
+type priorityQueue []*nodePair
+
+func (pq priorityQueue) Len() int           { return len(pq) }
+func (pq priorityQueue) Less(i, j int) bool { return pq[i].cosine > pq[j].cosine }
+func (pq priorityQueue) Swap(i, j int)      { pq[i], pq[j] = pq[j], pq[i] }
+
+func (pq *priorityQueue) Push(x interface{}) {
+	*pq = append(*pq, x.(*nodePair))
+}
+
+func (pq *priorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(*pq)
+	item := old[n-1]
+	old[n-1] = nil
+	*pq = old[:n-1]
+	return item
+}
+
 // buildInitialOrg builds the initial organization of the navigation graph.
 //
 // 1. Create nodes for all datasets and add them to graph.
 // 2. Create index over the nodes.
-// 3a. Query index for closest pair.
-// 3b. Remove closest pair of nodes from from index.
-// 3c. Created merged node from closest pair and add it to graph with edges to
+// 3. Query index with all nodes, add (query, NN) pairs to priority queue.
+// 4a. Pop closest pair of nodes off PQ and remove the nodes from index.
+// 4b. Created merged node from closest pair and add it to graph with edges to
 //     the pair of nodes.
-// 3d. Add the merged node to the index.
-// 3e. Repeat until no more nodes in index.
-func buildInitialOrg(db *database.DB) (*graph, error) {
+// 4c. Add the merged node to the index and PQ.
+// 4d. Repeat until index is empty.
+func buildInitialOrg(db *database.DB) (*tableGraph, error) {
 	g := newGraph()
 	if err := g.addDatasetNodes(db); err != nil {
 		return nil, err
@@ -191,22 +261,54 @@ func buildInitialOrg(db *database.DB) (*graph, error) {
 	}
 	defer index.delete()
 
-	for index.ntotal() > 1 {
-		a, b, err := index.closestPair()
+	var pq priorityQueue
+	pq, err = index.allPairs()
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		heap.Init(&pq)
+		pair := heap.Pop(&pq).(*nodePair)
+
+		if err := index.remove(pair.a, pair.b); err != nil {
+			return nil, err
+		}
+		node := g.addMergedNode(pair.a, pair.b)
+
+		if index.ntotal() == 0 {
+			g.root = node
+			break
+		}
+
+		p, err := index.query(node)
 		if err != nil {
 			return nil, err
 		}
-		if err := index.remove(a, b); err != nil {
-			return nil, err
-		}
-		id := g.NewNode().ID()
-		node := newMergedNode(id, a, b)
-		g.AddNode(node)
-		g.SetEdge(g.NewEdge(node, a))
-		g.SetEdge(g.NewEdge(node, b))
+		pq = append(pq, p)
 
 		if err := index.add(node); err != nil {
 			return nil, err
+		}
+
+		// Update PQ
+		for i := 0; i < len(pq); i++ {
+			p := pq[i]
+			// Remove pairs where a is in removed pair.
+			// Update pairs where b is in removed pair.
+			if p.a == pair.a || p.a == pair.b {
+				n := len(pq)
+				pq[i] = pq[n-1]
+				pq[n-1] = nil
+				pq = pq[:n-1]
+				i--
+			} else if p.b == pair.a || p.b == pair.b {
+				np, err := index.query_indexed(p.a)
+				if err != nil {
+					return nil, err
+				}
+				pq[i] = np
+			}
 		}
 	}
 	return g, nil
